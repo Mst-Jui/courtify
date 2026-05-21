bb307d390c5fbed10e0fba5e119de4f5cd33fa59
Mst-Jui <contact.juiakter@gmail.com>
2026-05-21 10:32:24 +0600

    font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,16 @@
 import dns from "node:dns";
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const josefin = Josefin_Sans({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -26,7 +22,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${josefin.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
